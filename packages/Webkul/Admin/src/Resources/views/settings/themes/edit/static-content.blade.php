6142136042794850ad137f656dc6a319d0ac11ed
grigorijs <Nightfall1988@github.com>
2024-09-23 13:30:19 +0300

    image adding in admin theme panel fixed

--- packages/Webkul/Admin/src/Resources/views/settings/themes/edit/static-content.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/themes/edit/static-content.blade.php
@@ -373,6 +373,7 @@
                     formData.append('{{ $currentLocale->code }}[options][][image]', selectedImage);
                     formData.append('id', "{{ $theme->id }}");
                     formData.append('type', "static_content");
+                    formData.append('locale', '{{ $currentLocale->code }}');
 
                     this.$axios.post('{{ route('admin.settings.themes.store') }}', formData)
                         .then((response) => {
@@ -380,7 +381,7 @@
 
                             let cursorPointer = editor.getCursor();
 
-                            editor.replaceRange(`<img class="lazy" data-src="${response.data}">`, {
+                            editor.replaceRange(`<div class="top-collection-card"><img style='visibility: visible;' class="lazy" width="396" height="396" src="${response.data}"><h3>Our Collections</h3></div>`, {
                                 line: cursorPointer.line, ch: cursorPointer.ch
                             });
 
